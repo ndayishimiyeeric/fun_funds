f51a6cb2ae6420b8e33f29f5d188e0ad07056523
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-01-13 07:46:42 +0100

    Add starter files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home, CampaignDetail, Profile, CreateCampaign } from './pages';
+import { Navbar, Sidebar } from './components';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-50">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+
+    </div>
   )
 }
 
---
+++ client/src/pages/CampaignDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CampaignDetail = () => {
+  return (
+    <div>CampaignDetail</div>
+  )
+}
+
+export default CampaignDetail
